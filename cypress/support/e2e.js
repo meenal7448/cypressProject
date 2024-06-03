38f36fd3822f0b562779fed812dce7e7a68d0993
Meenal Saxena <meenalsaxena@shashanks-MacBook-Air.local>
2024-06-03 12:11:52 +0530

    integratedAllure

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,6 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import 'allure-cypress/dist/commands.cjs'
+
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
